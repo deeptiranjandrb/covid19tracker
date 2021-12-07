a60234371900100304ddb79c64f93cac25771817
dbali00 <dbali00@bhnetwork.com>
2021-12-07 20:09:47 +0530

    changed card size

--- src/components/Cards/Cards.jsx
+++ src/components/Cards/Cards.jsx
@@ -11,7 +11,7 @@ const Cards = ({data:{confirmed, recovered, deaths, lastUpdate}}) => {
     return (
         <div className={styles.container}>
             <Grid container spacing={3} justify="center">
-                <Grid item component={Card} xs={12} md={4} className={cx(styles.card, styles.infected)}>
+                <Grid item component={Card} xs={12} md={3} className={cx(styles.card, styles.infected)}>
                 <CardContent>
                     <Typography color="textSecondary" gutterBottom>Infected</Typography>
                     <Typography variant="h5">
@@ -39,7 +39,7 @@ const Cards = ({data:{confirmed, recovered, deaths, lastUpdate}}) => {
                     <Typography variant="body2">Number of recoveries from COVID-19</Typography>
                 </CardContent>
                 </Grid>
-                <Grid item component={Card} xs={12} md={4} className={cx(styles.card, styles.deaths)} >
+                <Grid item component={Card} xs={12} md={3} className={cx(styles.card, styles.deaths)} >
                 <CardContent>
                     <Typography color="textSecondary" gutterBottom>Death</Typography>
                     <Typography variant="h5">
